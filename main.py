5cc8b70562ab38fa5ba4d4dcf374c694b7f29967
Ángel <angelcolinaalvarez@gmail.com>
2024-08-07 17:05:50 -0400

    Feat: Add a calculate_mines function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 class Game:
     def __init__(self):
@@ -8,9 +9,10 @@ class Game:
         os.system('cls' if os.name == 'nt' else 'clear')
 
 class Board:
-    def __init__(self, rows=10, cols=10):
+    def __init__(self, rows=9, cols=9):
         self.rows = rows
-        self.columns = cols
+        self.cols = cols
+        self.mines = self.calculate_mines()
         # self.game_board = [[(row, column) for column in range(1, 10)] for row in range(1, 10)]
         self.game_board = [[Box(row, column) for column in range(cols)] for row in range(rows)]
         
@@ -18,13 +20,30 @@ class Board:
 
         self.print_board()
 
-    def print_board(self):
-        for row in self.game_board:
-            print(' | '.join(str(box) for box in row))
-            if not (row == self.game_board[-1]):
-                print(' | '.join("-" * (len(str(box))) for box in row))
+    def print_board(self, style="fancy"):
+        if style == "fancy":
+            for row in self.game_board:
+                print(' | '.join(str(box) for box in row))
+                if not (row == self.game_board[-1]):
+                    print(' | '.join("-" * (len(str(box))) for box in row))
+        elif style == "simple":
+            for row in self.game_board:
+                print()
+                for box in row:
+                    print(str(box), end=" ")
 
-    # OUTDATED
+    def calculate_mines(self):
+        mines = int(round((self.rows * self.cols) * 0.2))
+        return mines
+    
+    def place_mines(self):
+        mines_placed = 0
+        while mines_placed < self.mines:
+            
+            pass
+        pass
+
+    # OUTDATED - CURRENTLY USELESS
     def search_box(self, row, column):
         x = row - 1
         y = column - 1
@@ -44,15 +63,24 @@ class Box:
         self.flag = False
         self.adjacents_mines = None
 
+    def reveal(self):
+        self.revealed = True
+
+    def put_flag(self):
+        self.flag = True
+
+    def remove_flag(self):
+        self.flag = False
+
+    def put_mine(self):
+        self.mine = True
+
     def __str__(self) -> str:
         return "□"
 
 class Pointer:
     def __init__(self) -> None:
         pass
-
-    def __str__(self) -> str:
-        return ""
 
 if __name__ == "__main__":
     game = Game()
